c23b41d390b9a5bab2af058249034128215f09e2
arb1ona <arbiona.shahu@gmail.com>
2021-01-15 01:54:21 +0100

    Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import CheckoutForm from "./components/CheckoutForm";
+
+function App() {
+  return (
+    <div className="App">
+      <CheckoutForm />
+    </div>
+  );
+}
+
+export default App;
